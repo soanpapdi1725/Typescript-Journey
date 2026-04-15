b3d5728d1830fa54f83723a0881f184e54c6b0e0
Sonu Yadav <sy239370@gmail.com>
2026-04-15 14:30:18 +0530

    Basic of Typescript

---
+++ Basic/basic.js
@@ -0,0 +1,9 @@
+function greet1(name) {
+  return ("Hello", name);
+}
+
+console.log(greet("Sonu"));
+console.log(greet(42));
+console.log(greet(true));
+//Name can be anything such as String, number or boolean
+// To Resolve this we need TypeSafety
